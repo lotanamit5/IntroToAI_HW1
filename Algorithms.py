df7646c76684171749f085f591e7858910c56c42
liorvardi <lior1khtur@gmail.com>
2023-05-05 14:58:15 +0300

    BFS done, started DFS

--- Algorithms.py
+++ Algorithms.py
@@ -7,7 +7,7 @@ import heapdict
 class Node():
     def __init__(self) -> None:
         self.state = -1
-        self.actions = None
+        self.actions = []
         
     @staticmethod
     def contains_state(node_list, node):
@@ -17,7 +17,7 @@ class Node():
         return False
     
     @staticmethod   
-    def make_node(state, actions):
+    def make_node(state, actions=[]):
         n = Node()
         n.state = state
         n.actions = actions
@@ -26,23 +26,29 @@ class Node():
 class Agent():
     def __init__(self) -> None:
         self.open = []
-        self.close = {}
-        self.actions = []
+        self.close = set()
+        self.actionsMapping = ['down','right','up','left']
         self.cost = 0
         self.expended = 0
         self.env = None
+        self.expanded_counter = 0
     
-    def reverse_action(self, action):
-        return (action + 2) % 4
-    
+    # def reverse_action(self, action):
+    #     return (action + 2) % 4
 
     @abstractmethod
     def next(self):
         pass
     
     @abstractmethod
-    def expand(self, current):
+    def insert_new(self, child):
         pass
+
+    def expand(self, current):    
+        self.expanded_counter += 1
+        for action, (state, cost, terminated) in self.env.succ(current.state).items():
+            if(state != None):
+                yield action, (state, cost)
     
     def solution(self, final_node):
         actions = final_node.actions
@@ -53,56 +59,88 @@ class Agent():
             self.env.set_state(state)
             total_cost += cost
             
-        return actions, total_cost, len(self.close)
-    
-    @abstractmethod
-    def search(self, env: FrozenLakeEnv) -> Tuple[List[int], int, float]:
-        pass
-    
-    
-class BFSAgent():
-    def __init__(self) -> None:
-        super().__init__()
-
-    def next(self):
-        return self.open.popleft()
-    
-    def expand(self, current):
-        for action, (state, cost, terminated) in self.env.succ(current.value):
-            if not terminated:
-                yield action, (state, cost)
+        return actions, total_cost, self.expanded_counter
     
     def search(self, env: FrozenLakeEnv) -> Tuple[List[int], int, float]:
         self.env = env
         self.env.reset()
         
-        node = Node.make_node(env.get_initial_state(), None)
+        node = Node.make_node(env.get_initial_state())
         if env.is_final_state(node.state):
             return self.solution(node)
         
         self.open.append(node)
-        self.close = {}
+        self.close = set()
         
-        while not self.open.isEmpty():
+        while len(self.open) > 0:
             node = self.next() # TODO: maybe refactor
             self.close.add(node.state)
-            for a, (s, _) in self.expand(node.state):
-                child = Node.make_node(s, node.actions + a)
+            for a, (s, _) in self.expand(node):
+                child = Node.make_node(s, node.actions + [a])
                 if child.state not in self.close and not Node.contains_state(self.open, child):
                     if env.is_final_state(s):
+                        # print(f"{self.close}")
                         return self.solution(child)
-                    self.open.append(child)
                     
+                    self.insert_new(child)
+        # print(f"{self.close}")        
         return None
-
-class DFSAgent():
+    
+    
+class BFSAgent(Agent):
     def __init__(self) -> None:
-        raise NotImplementedError
+        super().__init__()
+
+    def next(self):
+        return self.open.pop(0)
+    
+    def insert_new(self, child):
+        self.open.append(child)
+    
+
+
+class DFSAgent(Agent):
+    def __init__(self) -> None:
+        super().__init__()
+        self.states_proccessed = 0
+
+    def next(self):
+        return self.open.pop(-1)
+    
+    def insert_new(self, child):
+        self.open.append(child)
+
+    def see_state(self, state):
+        self.close.add(state)
+        self.states_proccessed += 1
+
+    def solution(self, node):
+        res = super().solution(node)
+        return res[0], res[1], self.states_proccessed
 
     def search(self, env: FrozenLakeEnv) -> Tuple[List[int], int, float]:
-        raise NotImplementedError
+        self.env = env
+        self.env.reset()
         
+        node = Node.make_node(env.get_initial_state())
+        # if env.is_final_state(node.state):
+            # return self.solution(node)
+        # open = [node]
+        self.close = set()
+        return self.search_aux(node)
 
+    def search_aux(self, node):
+        if self.env.is_final_state(node.state):
+            return self.solution(node)
+        self.see_state(node.state)
+        for a, (s, _) in self.expand(node):
+            child =  Node.make_node(s, node.actions + [a])
+            if child.state not in self.close:
+                res = self.search_aux(child)
+                if res is not None:
+                    return res
+        self.close.remove(node.state)
+        return None
 
 class UCSAgent():
   
