04b94d118679a533404b3a5d50b4488409269df2
lotanamit5 <lotan.amit@gmail.com>
2023-05-14 21:07:32 +0300

    wA* done, with small bug when w=0

--- Algorithms.py
+++ Algorithms.py
@@ -3,9 +3,8 @@ from collections import deque
 from abc import abstractmethod
 from FrozenLakeEnv import FrozenLakeEnv
 from typing import List, Tuple
+import math
 import heapdict
-
-VERBOSE = False
 
 class Node:
     def __init__(self, state, parent=None,action=0, cost=0, terminated=False):
@@ -14,10 +13,12 @@ class Node:
         self.action = action
         self.cost = cost
         self.terminated = terminated
+        self.g = parent.g + cost if parent is not None else cost
         
     def __repr__(self) -> str:
         return f"{self.state}"
-    
+
+  
 class Agent:
     def __init__(self):
         self.env: FrozenLakeEnv = None
@@ -26,8 +27,6 @@ class Agent:
         self.expanded: int= 0
         
     def expand(self, node: Node) -> List[Node]:
-        if VERBOSE: print(f"expanding node {node.state}")
-        
         self.expanded += 1
         
         for action, (state, cost, terminated) in self.env.succ(node.state).items():
@@ -46,7 +45,7 @@ class Agent:
             actions.append(node.action)
             node = node.parent
             
-        return reversed(actions), total_cost, self.expanded
+        return list(reversed(actions)), total_cost, self.expanded
     
     def init_search(self, env: FrozenLakeEnv):
         self.env = env
@@ -57,7 +56,8 @@ class Agent:
     @abstractmethod    
     def search(self, env: FrozenLakeEnv) -> Tuple[List[int], int, float]:
         pass
-    
+
+  
 class BFSAgent(Agent):
     def __init__(self):
         super().__init__()
@@ -75,15 +75,14 @@ class BFSAgent(Agent):
         while len(self.open) > 0:
             node = self.open.popleft()
             self.close.add(node.state)
-            if VERBOSE: print(f"close: {[s for s in self.close]}")
             
             for child in self.expand(node):
                 if child.state not in self.close and child.state not in [n.state for n in self.open]:
                     if self.env.is_final_state(child.state):
                         return self.solution(child)
                     self.open.append(child)
-                    if VERBOSE: print(f"open: {[s for s in self.open]}")
-             
+
+            
 class DFSAgent(Agent):
     def __init__(self):
         super().__init__()
@@ -114,6 +113,7 @@ class DFSAgent(Agent):
         
         return self.recursive_dfs()
 
+
 class UCSAgent(Agent):
     def __init__(self):
         super().__init__()
@@ -126,20 +126,28 @@ class UCSAgent(Agent):
         
         if self.env.is_final_state(node.state):
             return self.solution(node)
-        self.open[node] = (node.cost, node.state)
+        self.open[node] = (node.g, node.state)
         
         while len(self.open) > 0:
-            node, (cost, _) = self.open.popitem()
+            node, _ = self.open.popitem()
             self.close.add(node.state)
-            if VERBOSE: print(f"close: {[s for s in self.close]}")
             
             for child in self.expand(node):
-                if child.state not in self.close and child.state not in [n[1] for n in self.open.values()]:
+                OPEN = [n.state for n in self.open.keys()]
+                if child.state not in self.close and child.state not in OPEN:
                     if self.env.is_final_state(child.state):
                         return self.solution(child)
-                    self.open[child] = (cost + child.cost, child.state)
-                    if VERBOSE: print(f"open: {[s for s in self.open]}")
-    
+                    self.open[child] = (child.g, child.state)
+
+
+class InformedNode(Node):
+    def __init__(self, state, parent=None, action=0, cost=0, terminated=False,
+                 h=0, f=0):
+        super().__init__(state, parent=parent, action=action, cost=cost, terminated=terminated)
+        self.h = h
+        self.f = f
+
+
 class InformedAgent(Agent):
     def __init__(self):
         super().__init__()
@@ -154,6 +162,19 @@ class InformedAgent(Agent):
         ]
         return min(manhatans + [100])
     
+    def f(self, node: InformedNode) -> float:
+        return node.g + self.h(node.state)
+    
+    def expand(self, node: InformedNode) -> List[InformedNode]:
+        self.expanded += 1
+        
+        for action, (state, cost, terminated) in self.env.succ(node.state).items():
+            if state != None:
+                child = InformedNode(state, parent=node, action=action, cost=cost, terminated=terminated, h=self.h(state))
+                child.f = self.f(child)
+                yield child
+    
+   
 class GreedyAgent(InformedAgent):
     def __init__(self):
         super().__init__()
@@ -162,8 +183,8 @@ class GreedyAgent(InformedAgent):
         self.init_search(env)
         self.open: heapdict = heapdict.heapdict()
 
-        node = Node(self.env.get_initial_state())
-        self.open[node] = (self.h(node.state), node.state)
+        node = InformedNode(self.env.get_initial_state(), h=self.h(self.env.get_initial_state()))
+        self.open[node] = (node.h, node.state)
 
         while len(self.open) > 0:
             node, _ = self.open.popitem()
@@ -171,54 +192,75 @@ class GreedyAgent(InformedAgent):
                 return self.solution(node)
 
             for child in self.expand(node):
-                if child.state not in self.close and child.state not in [n[1] for n in self.open.values()]:
-                    self.open[child] = (self.h(child.state), child.state)
+                open = [n[1] for n in self.open.values()]
+                if child.state not in self.close and child.state not in open:
+                    self.open[child] = (child.h, child.state)
 
         return None
-    
+
+
 class WeightedAStarAgent(InformedAgent):
     def __init__(self):
         super().__init__()
     
-    def f(self, node: Node) -> float:
-        return (1-self.h_weight)*node.g + self.h_weight*self.h(node.state)
+    def f(self, node: InformedNode) -> float:
+        w = self.h_weight
+        if w == 1: # avoid g==inf problem
+            return node.h
+        f = (1 - w) * node.g + w * node.h
+        return f
     
-    def update_node(self, state: int, parent: Node, g: float, f: float) -> Node:
-        pass
+    def update_node(self, node: InformedNode, parent: InformedNode, g: float) -> InformedNode:
+        return InformedNode(node.state, parent=parent, action=node.action, cost=node.cost,
+                            terminated=node.terminated, g=g, h=self.h(node.state), f=self.f(node))
     
     def search(self, env: FrozenLakeEnv, h_weight=0.5) -> Tuple[List[int], int, float]:
-        self.init_search(env)
+        assert 0 <= h_weight <= 1, "h_weight must be between 0 and 1"
         self.h_weight = h_weight
+        
+        self.init_search(env)
         self.open: heapdict = heapdict.heapdict()
         
-        node = Node(self.env.get_initial_state())
-        self.open[node] = (self.f(node), node.state)
+        init_state = self.env.get_initial_state()
+        node = InformedNode(init_state, h=self.h(init_state),g=0)
+        node.f = self.f(node)
+        self.open[node] = (node.f, node.state)
         
         while len(self.open) > 0:
             node, _ = self.open.popitem()
             
-            self.close.add(node.state)
+            self.close.add(node)
             
             if env.is_final_state(node.state):
                 return self.solution(node)
             
             for child in self.expand(node):
                 new_g = node.g + child.cost
-                open = [n[1] for n in self.open.values()]
+                OPEN = [n for n in self.open.keys() if n.state == child.state]
+                CLOSE = [n for n in self.close if n.state == child.state]
                 
-                if child.state not in self.close and child.state not in open:
-                    self.open[child] = (self.f(child), child.state)
+                if len(CLOSE) == 0 and len(OPEN) == 0:
+                    self.open[child] = (child.f, child.state)
                     
-                elif child.state in open:
-                    n_curr = next(filter(lambda n, _: n.state == child.state, self.open.keys()), None)[0]
+                elif len(OPEN) > 0:
+                    n_curr = OPEN[0]
                     if new_g < n_curr.g:
-                        n_curr = self.update_node(child.state, node, new_g, new_g + self.h(child.state))
-                        self.open.update_key(n_curr)
+                        n_curr = self.update_node(child, parent=node, g=new_g)
+                        self.open[n_curr] = (n_curr.f, n_curr.state)
+                        for n in OPEN:
+                            del self.open[n]
                         
-                else:  # child.state in close
-                    n_curr = next(filter(lambda n, _: n.state == child.state, self.close))
+                else:  # child in close
+                    n_curr = CLOSE[0]
                     if new_g < n_curr.g:
-                        n_curr = self.update_node(child.state, node, new_g, new_g + self.h(child.state))
-                        self.open.update_key(n_curr)
-                        open[n_curr] = (self.f(n_curr), n_curr.state)
+                        n_curr = self.update_node(child, parent=node, g=new_g)
+                        OPEN[n_curr] = (n_curr.f, n_curr.state)
                         self.close.remove(n_curr)
+
+
+class IDAStarAgent(InformedAgent):
+    def __init__(self):
+        super().__init__()
+    
+    def search(self, env: FrozenLakeEnv) -> Tuple[List[int], int, float]:
+        pass
